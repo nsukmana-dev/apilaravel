a3f26e114dcb04b372bc1adee5f4fd90e768d1b4
nsukmana-dev <nsukmana.dev@gmail.com>
2023-12-20 14:25:42 +0700

    setup controller

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegistrationRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        
+    }
+
+    public function register(RegistrationRequest $request)
+    {
+        
+    }
+}
